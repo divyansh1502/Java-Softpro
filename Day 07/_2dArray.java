567d171f247b70c5577dd2e53ee98f80cef03543
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-07-07 17:06:33 +0530

    2D array

---
+++ Day 07/_2dArray.java
@@ -0,0 +1,10 @@
+
+// public class _2dArray {
+//     public static void main(String[] args) {
+//         int[][] arr = new int[3][3];
+//         int[][] a = {
+//                         1,2,
+//                         3,4
+//                     }
+//     }
+// }
